5d977ebabd85d2248d7be36d06a39f46ac918795
Roberth González <reliutg@gmail.com>
2023-11-25 12:56:35 +0100

    Refactor middleware.ts and remove language.json and study.json

--- middleware.ts
+++ middleware.ts
@@ -2,11 +2,9 @@ import { NextResponse } from "next/server"
 import type { NextRequest } from "next/server"
 import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs"
 
-import type { Database } from "@/lib/types/database"
-
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next()
-  const supabase = createMiddlewareClient<Database>({ req, res })
+  const supabase = createMiddlewareClient({ req, res })
 
   const { data } = await supabase.auth.getSession()
 
